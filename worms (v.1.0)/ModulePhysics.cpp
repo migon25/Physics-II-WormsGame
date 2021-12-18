901686feb2e4afe6310303bb3072b0e8e3d5b609
Xymaru <xyma.ru99@gmail.com>
2021-12-18 16:59:59 +0100

    Remove box2d

--- worms (v.1.0)/ModulePhysics.cpp
+++ worms (v.1.0)/ModulePhysics.cpp
@@ -9,12 +9,6 @@
 #include <cmath>
 #include "Collider.h"
 
-#ifdef _DEBUG
-#pragma comment( lib, "Box2D/libx86/Debug/Box2D.lib" )
-#else
-#pragma comment( lib, "Box2D/libx86/Release/Box2D.lib" )
-#endif
-
 ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 	for (uint i = 0; i < MAX_PHYSICS_BODIES; ++i)
